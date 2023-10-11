68521cc3f0f010b5c84ae669959c6ceb4cd557c8
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-10-11 16:28:41 +0200

    emialjs try fail

--- contact.js
+++ contact.js
@@ -1,32 +1,21 @@
-const contactForm = document.querySelector("#contact-form");
-const submitBtn = document.querySelector(".submit-btn");
-const nameInput = document.querySelector("#user_name");
-const emailInput = document.querySelector("#user_email");
-const messageInput = document.querySelector("#message");
+function sendMail() {
+  var params = {
+    name: document.getElementById("user_name").value,
+    email: document.getElementById("user_email").value,
+    message: document.getElementById("message").value,
+  };
 
-const publicKey = "tMJ6iLYRPLiPo_2pi";
-const serviceID = "my_site";
-const templateID = "template_mysite";
+  const serviceID = "my_site";
+  const templateID = "template_mysite";
 
-emailjs.init(publicKey);
-
-contactForm.addEventListener("submit"), e =>
-  e.preventDefault();
-  submitBtn.innerText = "Just a moment...";
-
-const inputFields = {
-  name: nameInput.value,
-  email: emailInput.value,
-  message: messageInput.value
+  emailjs
+    .send(serviceID,templateID,params)
+    .then(res) = {
+      document:getElementById("user_name").value = "",
+      document:getElementById("user_email").value = "",
+      document:getElementById("message").value = "",
+      console:log(res),
+      alert:("Your message sent successfully")
+    }
+    .catch((err) => console.log(err));
 }
-
-emailjs.send(serviceID, templateID, inputFields)
-  .then(() => {
-    submitBtn.innerText = "Message sent successfully";
-    nameInput.value = "";
-    emailInput.value = "";
-    messageInput.value = "";
-  }, (error) => {
-      console.log(error);
-      submitBtn.innerText = "Somthing went wrong";
-  });
